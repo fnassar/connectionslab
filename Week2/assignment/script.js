1021abd5e9b69f1eba8b1db979b18a47f5af07e7
fnassar <75119980+fnassar@users.noreply.github.com>
2022-02-07 12:21:56 +0400

    .

--- Week2/assignment/script.js
+++ Week2/assignment/script.js
@@ -26,22 +26,22 @@ window.addEventListener('load', function() {
     //inputs
     let input_name = this.document.getElementById('name');
     let input_link = this.document.getElementById('link');
-
+    let name;
+    let link;
     input_name.addEventListener('change', function(e1) {
-        let name = e1.target.value;
-        input_link.addEventListener('change', function(e2) {
-            let link = e2.target.value;
-            let listItem = document.createElement('li');
-            listItem.innerHTML = "<a href=\"" + link + "\">" + name + "<a>";
-            console.log("<a href=\"" + link + "\">" + name + "<a>");
-            console.log(listItem);
+        name = e1.target.value;
+    })
+    input_link.addEventListener('change', function(e2) {
+
+        link = e2.target.value;
+        let listItem = document.createElement('li');
+        listItem.innerHTML = "<a href=\"" + link + "\">" + name + "<a>";
+        // console.log("<a href=\"" + link + "\">" + name + "<a>");
+        console.log(listItem);
 
 
-            let list = document.getElementById('list');
-            list.appendChild(listItem);
-
-        })
-
+        let list = document.getElementById('list');
+        list.appendChild(listItem);
 
     })
 })
