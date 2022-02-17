8f6385184a37d927e73eaad230952fbc6be1c532
fnassar <75119980+fnassar@users.noreply.github.com>
2022-02-17 12:05:24 +0400

    .

--- Week4/inclass/script.js
+++ Week4/inclass/script.js
@@ -1,9 +1,72 @@
-fetch("http://api.open-notify.org/astros.json", )
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        // 
-    })
-    .catch(err => {
-        console.error(err);
-    });
+let url = "http://api.open-notify.org/astros.json";
+let astrodata;
+let dataReady = false;
+let astronuts = [];
+
+window.addEventListener('load', () => {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            astrodata = data;
+            dataReady = true;
+            for (let i = 0; i < astrodata.people.length; i++) {
+                astronuts.push(new Astronut(astrodata.people[i].name, astrodata.people[i].craft, i));
+            }
+            // 
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+});
+
+
+
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    background(0, 0, 50);
+
+}
+
+function draw() {
+    background(0, 0, 50);
+
+    if (dataReady) {
+        for (let i = 0; i < astrodata.people.length; i++) {
+            astronuts[i].draw();
+            astronuts[i].showName(mouseX, mouseY);
+        }
+    }
+}
+
+
+
+
+class Astronut {
+    constructor(name, craft, n) {
+        this.name = name;
+        this.craft = craft;
+        this.x = n * innerWidth / 10 + innerWidth / 20;
+        this.y = innerHeight / 2;
+        // this.
+        // this.randx = Math.random() * 350 + 10;
+        // this.randy = Math.random() * 350 + 10;
+        // this.randR = Math.random() * 100 + 10;
+    }
+    draw() {
+        ellipseMode(CENTER);
+        fill(100);
+        noStroke();
+        ellipse(this.x, this.y, 20, 20);
+    }
+    showName(mx, my) {
+        if (dist(mx, my, this.x, this.y) <= 10) {
+            // textMode(CENTER);
+            fill(255);
+            text(this.name, this.x, this.y);
+        }
+    }
+
+}
--- week3/assignment/script.js
+++ week3/assignment/script.js
@@ -6,84 +6,103 @@ let city, country;
 locLink = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?location=" + lat + "%2B" + lng;
 // " + lat + "&lng=" + lng;
 
-fetch(locLink, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-            "x-rapidapi-key": "6c41fb0e8dmsha569b026441c479p15b86ajsnd0a8cea9e716"
-        }
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        // console.log(data.data[0].country, data.data[0].name);
-        country = data.data[0].country.replace(/ /g, "%20");
-        city = data.data[0].name.replace(/ /g, "%20");
-        searchLink = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=" + city + "%20" + country + "&pageNumber=1&pageSize=5&autoCorrect=true"
-        console.log(searchLink);
-        fetch(searchLink, {
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
-                    "x-rapidapi-key": "6c41fb0e8dmsha569b026441c479p15b86ajsnd0a8cea9e716"
-                }
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                console.log(data.value[0].url);
-                console.log(data.value[1].url);
-                console.log(data.value[2].url);
-                console.log(data.value[3].url);
-                console.log(data.value[4].url);
+window.addEventListener('load', () => {
+    fetch(locLink, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+                "x-rapidapi-key": "6c41fb0e8dmsha569b026441c479p15b86ajsnd0a8cea9e716"
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            // console.log(data.data[0].country, data.data[0].name);
+            country = data.data[0].country.replace(/ /g, "%20");
+            city = data.data[0].name.replace(/ /g, "%20");
+            searchLink = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=" + city + "%20" + country + "&pageNumber=1&pageSize=5&autoCorrect=true"
+            console.log(searchLink);
+            fetch(searchLink, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+                        "x-rapidapi-key": "6c41fb0e8dmsha569b026441c479p15b86ajsnd0a8cea9e716"
+                    }
+                })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                    console.log(data.value[0].url);
+                    console.log(data.value[1].url);
+                    console.log(data.value[2].url);
+                    console.log(data.value[3].url);
+                    console.log(data.value[4].url);
 
-                let img = document.createElement("img");
-                img.src = data.value[1].url;
-                img.alt = "image unavailable."
-                console.log(img);
-                img.style.maxHeight = '100%';
-                img.style.maxWidth = '100%';
-                document.getElementById("image").appendChild(img);
-                let img2 = document.createElement("img");
-                img2.src = data.value[2].url;
-                img2.alt = "image unavailable."
-                console.log(img2);
-                img2.style.maxHeight = '100%';
-                img2.style.maxWidth = '100%';
-                img2.style.display = 'none';
-                document.getElementById("image").appendChild(img2);
-                let img3 = document.createElement("img");
-                img3.src = data.value[3].url;
-                img3.alt = "image unavailable."
-                console.log(img3);
-                img3.style.maxHeight = '100%';
-                img3.style.maxWidth = '100%';
-                img3.style.display = 'none';
-                document.getElementById("image").appendChild(img3);
-                let img4 = document.createElement("img");
-                img4.src = data.value[4].url;
-                img4.alt = "image unavailable."
-                console.log(img4);
-                img4.style.maxHeight = '100%';
-                img4.style.maxWidth = '100%';
-                img4.style.display = 'none';
-                document.getElementById("image").appendChild(img4);
+                    let img = document.createElement("img");
+                    img.src = data.value[1].url;
+                    img.alt = "image unavailable."
+                    console.log(img);
+                    img.style.maxHeight = '100%';
+                    img.style.maxWidth = '100%';
+                    document.getElementById("image").appendChild(img);
+                    let img2 = document.createElement("img");
+                    img2.src = data.value[2].url;
+                    img2.alt = "image unavailable."
+                    console.log(img2);
+                    img2.style.maxHeight = '100%';
+                    img2.style.maxWidth = '100%';
+                    img2.style.display = 'none';
+                    document.getElementById("image").appendChild(img2);
+                    let img3 = document.createElement("img");
+                    img3.src = data.value[3].url;
+                    img3.alt = "image unavailable."
+                    console.log(img3);
+                    img3.style.maxHeight = '100%';
+                    img3.style.maxWidth = '100%';
+                    img3.style.display = 'none';
+                    document.getElementById("image").appendChild(img3);
+                    let img4 = document.createElement("img");
+                    img4.src = data.value[4].url;
+                    img4.alt = "image unavailable."
+                    console.log(img4);
+                    img4.style.maxHeight = '100%';
+                    img4.style.maxWidth = '100%';
+                    img4.style.display = 'none';
+                    document.getElementById("image").appendChild(img4);
 
 
-            })
-            .catch(err => {
-                console.error(err);
-            });
+                })
+                .catch(err => {
+                    console.error(err);
+                });
 
-        // 
-    })
-    .catch(err => {
-        console.error(err);
-    });
+            // 
+        })
+        .catch(err => {
+            console.error(err);
+        });
+})
 
-//this is an image search engine
+let worldmap = document.getElementById("worldmap");
 
-// P5 Code here
+let canvasWidth = worldmap.offsetWidth;
+let canvasHeight = worldmap.offsetHeight;
+
+function setup() {
+    //Create the canvas and save it to a variable;
+    const myCanvas = createCanvas(canvasWidth, canvasHeight);
+    //Set the parent of the canvas to an exisitng html element's id value 
+    myCanvas.parent("worldmap");
+    background(220, 40, 50);
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+    ellipse(mouseX, mouseY, 30, 30);
+}
 
 
 
