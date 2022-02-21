2cb8855c93238b9f6aad1ee1c3398106e0a336a9
fnassar <75119980+fnassar@users.noreply.github.com>
2022-02-21 19:07:44 +0400

    .

--- Week4/assignment_1test/script.js
+++ Week4/assignment_1test/script.js
@@ -1,10 +1,12 @@
 let city, country;
 let imgs = [];
+let titles = ["name", "capital", "region", "currency", "iso2", "gdp", "population", "pop_density", "surface_area", "co2_emissions", "pop_growth", "internet_users", "refugees", "tourists", "urban_population", "unemployment", "urban_population_growth", "life_expectancy_female", "life_expectancy_male", "fertility", "threatened_species", "homicide_rate", "infant_mortality", "post_secondary_enrollment_female", "post_secondary_enrollment_male", "primary_school_enrollment_female", "primary_school_enrollment_male", "secondary_school_enrollment_female", "secondary_school_enrollment_male", "exports", "imports", "sex_ratio", "employment_agriculture", "employment_industry", "employment_services", "forested_area", "gdp_growth", "gdp_per_capita"];
 let str = "blabla";
 let tag;
-let locLink, searchLink;
+let locLink, searchLink, datalink;
 let lng, lat, sign;
 let imagecounter = 0;
+let done = false;
 
 window.addEventListener("load", () => {
     window.scrollTo({ top: 0 });
@@ -26,7 +28,6 @@ window.addEventListener("load", () => {
     function onMapClick(e) {
         clng = e.latlng.lng;
         clat = e.latlng.lat;
-        console.log(clat, clng);
         // call function to get city name
         // popup
         //     .setLatLng(e.latlng)
@@ -47,9 +48,7 @@ window.addEventListener("load", () => {
             sign = "%2B"
             lng = clng;
         }
-        // console.log("bla", lat, lng);
         // tag = document.getElementById('tag');
-        // console.log(tag);
         // tag.addEventListener('click', scrolltolem('content'));
 
     }
@@ -63,7 +62,6 @@ window.addEventListener("load", () => {
 
 function scrolltoelem(id) {
     locLink = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?location=" + lat + sign + lng;
-    console.log(locLink);
     // change mouse shape to load
     document.body.style.cursor = "wait";
     document.getElementById("tag").style.cursor = "wait";
@@ -76,20 +74,14 @@ function scrolltoelem(id) {
         })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             if (data.data.length != 0) {
                 imgs = [];
                 // fetch required data
-                country = data.data[0].country.replace(/ /g, "%20");
-                city = data.data[0].name.replace(/ /g, "%20");
-                console.log(country, city);
-
-                getflag(country);
-
+                country = data.data[0].country;
+                city = data.data[0].name;
+                console.log(country);
+                printdata(country, id);
                 //done with fetches?
-                document.body.style.cursor = "default";
-                document.getElementById("tag").style.cursor = "pointer";
-                document.getElementById(id).scrollIntoView({ behavior: "smooth" });
             } else {
                 document.body.style.cursor = "default";
                 document.getElementById("tag").style.cursor = "pointer";
@@ -105,7 +97,29 @@ function scrolltoelem(id) {
 
 }
 
-function getflag(countryname) {
+function printdata(country, id) {
+    // let country = countryname.replace(/%20/g, " ");
+    datalink = "https://api.api-ninjas.com/v1/country?name=";
+    // https://api.api-ninjas.com/v1/country?name=United States
+
+    fetch(datalink + country, {
+            "method": "GET",
+            "headers": {
+                'X-Api-Key': 'NPa7JmMWM73DEsQATsWcXg==pZ8jsMNGt6wpukSN'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data[0]);
+            printdetails(data[0]);
+            printflag(data[0].iso2, id);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+function printflag(countryname, id) {
     // "https://countryflagsapi.com"
     imgs.push(document.createElement("img"));
     imgs[0].src = "https://countryflagsapi.com/png/" + countryname;
@@ -117,12 +131,20 @@ function getflag(countryname) {
     } else {
         document.getElementById('flagimg').replaceWith(imgs[0]);
     }
+    //last step
+    scrollfunction(id);
     imagecounter++;
-    console.log("done", "https://countryflagsapi.com/png/" + countryname);
-
-
 }
 
-function getdata(countryname) {
+function scrollfunction(id) {
+    let seconds = new Date().getTime();
 
+    document.body.style.cursor = "default";
+    document.getElementById("tag").style.cursor = "pointer";
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+function printdetails(data) {
+    // loop data.titles[i] and add to "info" id div
+    // make into new Li tags then append child
 }
