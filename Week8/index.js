92e51178988bc728e9473771a17a959b9eb92407
fnassar <75119980+fnassar@users.noreply.github.com>
2022-03-26 18:08:57 +0400

    sockets

--- Week8/index.js
+++ Week8/index.js
@@ -1,11 +1,31 @@
+// requires
+const { Socket } = require('dgram');
 let express = require('express');
 let http = require('http');
-let app = express();
-let server = http.createServer(app); // create http server
+let io = require('socket.io');
 
-// 
+//  Create server
+// order !!IMPORTANT
+let app = express();
+let server = http.createServer(app); // wrap express app with http
+io = new io.Server(server); // use socket.io on the http app
+
+// start server comunication?
 app.use('/', express.static('public'));
 
+// 
+io.sockets.on('connection', (socket) => {
+    console.log("we have a new client: ", socket.id);
+    socket.on('disconnect', () => {
+        console.log("client: ", socket.id, "is disconnected");
+    })
+
+    socket.on('mousePosData', (data) => {
+        io.sockets.emit('mouseDataFromServer', data);
+    })
+})
+
+// 
 server.listen(8800, () => {
     console.log("server running at localHost:8800");
 })
--- Week8/public/script.js
+++ Week8/public/script.js
@@ -1,7 +1,40 @@
+let socket = io();
+
+let width = 400; //window.innerWidth * 0.8;
+let height = 400; //window.innerHeight * 0.8;
+
+let colorr;
+
+window.addEventListener('load', () => {
+    socket.on('connect', () => {
+        console.log("client connected via sockets");
+
+    })
+})
+
+// P5 Code
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, height);
+    background(220);
+    socket.on('mouseDataFromServer', (data) => {
+        // console.log(data);
+        drawEllipseWithData(data);
+    })
+    colorr = { r: (Math.random() * 255), g: (Math.random() * 255), b: (Math.random() * 255) };
 }
 
 function draw() {
-    background(220);
+    // 
+}
+
+// emit mouse position when mouse moves
+function mouseDragged() {
+    let mousePos = { x: round(mouseX), y: round(mouseY), c: colorr };
+    // ellipse(mousePos.x, mousePos.y, 10, 10); // send to server then display!!
+    socket.emit('mousePosData', mousePos); // GOLD !!IMPORTANT
+}
+
+function drawEllipseWithData(data) {
+    fill(data.c.r, data.c.g, data.c.b);
+    ellipse(data.x, data.y, 10, 10);
 }
