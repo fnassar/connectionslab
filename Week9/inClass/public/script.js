2682ae46cc1b06e87ec66c75238d5eadb6b0235f
fnassar <75119980+fnassar@users.noreply.github.com>
2022-03-31 11:29:59 +0400

    added chat+lobby

--- Week9/inClass/public/script.js
+++ Week9/inClass/public/script.js
@@ -1,16 +1,10 @@
 let socket = io();
 let chatBox = document.getElementById('chat-box-msg1');
 socket.on('connect', () => {
-    console.log("client connected via sockets");
+        console.log("client connected via sockets");
 
-})
-
-/*
-let newUsr = document.createElement('li');
-    newUsr.innerHTML = "new User has joined the chat";
-    chatBox.appendChild(newUsr);
-*/
-// recieve prev msgs
+    })
+    // recieve prev msgs
 socket.on('pastMessages', (data) => {
     console.log(data);
 
@@ -30,9 +24,12 @@ socket.on('chatMessage', (data) => {
 })
 
 window.addEventListener('load', () => {
-    let submitButton = document.getElementById('send-button');
+    let msgBox = document.getElementById('msg-input');
+    let chatForm = document.getElementById('chat-form');
 
-    submitButton.addEventListener('click', () => {
+    // e=event
+    chatForm.addEventListener('submit', (e) => {
+        e.preventDefault(); // stops eg enter to submit
         let name = document.getElementById('name-input').value;
         let msg = document.getElementById('msg-input').value;
 
@@ -43,58 +40,9 @@ window.addEventListener('load', () => {
 
         socket.emit('chatMessage', chatObj);
     })
+    msgBox.addEventListener('keypress', () => {
+            console.log('typing');
+            socket.emit('userTyping');
+        })
+        // msgBox.onkeypress = () => {}// old version not going to be always supported
 })
-
-
-/* 
-let socket = io();
-let chatBox = document.getElementById('chat-box-msg1');
-socket.on('connect', () => {
-    console.log("client connected via sockets");
-
-})
-// recieve prev msgs
-socket.on('pastMessages', (data) => {
-    console.log(data);
-
-    // load prev chats
-    for (let i = 0; i < data.oldMessages.length; i++) {
-        let msg = document.createElement('li');
-        msg.innerHTML = data.oldMessages[i].name + " : " + data.oldMessages[i].msg;
-        chatBox.appendChild(msg);
-    }
-})
-
-socket.on('chatMessage', (data) => {
-    console.log(data);
-    let msg = document.createElement('li');
-    msg.innerHTML = data.name + " : " + data.msg;
-    chatBox.appendChild(msg);
-})
-
-window.addEventListener('load', () => {
-    let submitButton = document.getElementById('send-button');
-    let msgBox = document.getElementById('msg-input');
-    msg.addEventListener('change', () => {
-        socket.emit('userTyping');
-    });
-
-
-    submitButton.addEventListener('click', () => {
-        let name = document.getElementById('name-input').value;
-        let msg = msgBox.value;
-
-        chatObj = {
-            'name': name,
-            'msg': msg
-        }
-        
-        socket.emit('chatMessage', chatObj);
-    })
-
-    msgBox.onkeypress = () => {
-        console.log('typing');
-        socket.emit('userTyping');
-    }
-})
-*/
